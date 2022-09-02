7a11a15846b4647b3ab24099c10ac691f2c68c54
MarcoBeraldo <beraldomar@gmail.com>
2022-09-02 10:36:57 +0200

    fixed link and created genre selecet

--- v2/js/script.js
+++ v2/js/script.js
@@ -3,10 +3,11 @@ const app = new Vue({
   el: "#root",
   data: {
     discs: [],
+    selectedGenre: "",
   },
   methods: {
     fetchDiscs() {
-      axios.get("../../api/discs.php").then((res) => {
+      axios.get(`../api/discs.php?genre=${this.selectedGenre}`).then((res) => {
         this.discs = res.data;
       });
     },
